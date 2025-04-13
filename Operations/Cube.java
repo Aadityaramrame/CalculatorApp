d82c10f824a10b07fb666b0b35cd1d548d5d5db3
Aaditya Ramrame <161333408+Aadityaramrame@users.noreply.github.com>
2025-04-13 23:36:33 +0530

    Create Cube.java

---
+++ Operations/Cube.java
@@ -0,0 +1,21 @@
+//Cube.java
+// Declaring the package where this class belongs
+package Operations;
+
+// Importing Scanner class to read user input
+import java.util.Scanner;
+
+// Creating a public class named Cube
+public class Cube {
+    public static void perform(Scanner sc) {
+        // Asking the user to enter the number
+        System.out.print("Enter number: ");
+        double a = sc.nextDouble();
+
+        // Cubing the number
+        double result = a * a * a;
+
+        // Displaying the final result
+        System.out.println("Cube = " + result);
+    }
+}
